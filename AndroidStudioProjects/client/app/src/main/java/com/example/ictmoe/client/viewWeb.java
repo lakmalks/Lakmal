5605f09b1794a2a3a4a0f3642bb8504e887cdf1b
lakmalks <go2lakmal@gmail.com>
2018-05-10 15:28:16 +0530

    view

---
+++ AndroidStudioProjects/client/app/src/main/java/com/example/ictmoe/client/viewWeb.java
@@ -0,0 +1,13 @@
+package com.example.ictmoe.client;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class viewWeb extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_web);
+    }
+}
